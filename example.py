5198e4db1b2a70846a3e33921d6960cfb49779ac
Risteel <z3310z33104107@gmail.com>
2021-12-09 17:17:48 +0800

    save weight matching result and set n cluster

--- example.py
+++ example.py
@@ -1,11 +1,22 @@
+import os
+import pickle
 from utils import *
 from STSC.stsc import *
 
+
 if __name__ == '__main__':
-    iris_data, iris_outliers_index, iris_inliers_index = read_dataset('iris')
-    X, SQ = generate_X_SQ(iris_data, iris_outliers_index)
+    dataset_name = 'airquality'
+    data, outliers_index, inliers_index = read_dataset(dataset_name)
+    X, SQ = generate_X_SQ(data, outliers_index)
     G = generate_graph(X)
     V = get_V(G)
-    S = maximum_weight_matching(SQ, V)
-    C = self_tuning_spectral_clustering_np(S)
-    print(C)
+    if not os.path.isfile('outlier_weight_{}.npy'.format(dataset_name)):
+        S = maximum_weight_matching(SQ, V)
+        np.save("outlier_weight_{}".format(dataset_name), S)
+    else:
+        S = np.load("outlier_weight_{}.npy".format(dataset_name))
+    n_cluster = 8
+    C = self_tuning_spectral_clustering_np(S, n_cluster, n_cluster)
+    with open('{}_cluster_{}.pickle'.format(n_cluster, dataset_name), 'wb') as f:
+        pickle.dump(C, f)
+    print(len(C))
