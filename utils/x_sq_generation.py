e36080afa4da4faabdcf495e78a7b147a677dba9
Eric07110904 <azsx26735546@gmail.com>
2021-12-11 03:29:07 +0800

    update return values

--- utils/x_sq_generation.py
+++ utils/x_sq_generation.py
@@ -1,9 +1,8 @@
-from typing import Tuple
-
+from typing import Tuple, List
 import networkx as nx
 import numpy as np
 from node2vec import Node2Vec
-
+from copy import deepcopy
 __all__ = [
     'generate_X_SQ',
     'generate_graph',
@@ -24,20 +23,22 @@ def get_outliers_score(data: np.ndarray) -> np.ndarray:
     return scores
 
 
-def generate_X_SQ(data: np.ndarray, outliers_index: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+def generate_X_SQ(data: np.ndarray, outliers_index: np.ndarray) -> Tuple[np.ndarray, np.ndarray, List]:
     n_features = data.shape[1]
     n_outliers = len(outliers_index)
     m = n_outliers
     n = (n_features * (n_features - 1)) >> 1
     X: np.ndarray = np.empty((m, n), dtype=np.float64)
-    for idx, fp_id in enumerate(get_combinations(n_features)):
+    fp_record = get_combinations(n_features)
+    retv_fp = deepcopy(fp_record)
+    for idx, fp_id in enumerate(fp_record):
         scores = get_outliers_score(data[:, fp_id])[outliers_index]
         X[:, idx] = scores
     X_min = X.min()
     X_max = X.max()
     X: np.ndarray = (X - X_min) / (X_max - X_min)
     SQ = X.argsort(axis=1)[:, ::-1]
-    return X, SQ
+    return X, SQ, list(retv_fp)
 
 
 def generate_graph(X: np.ndarray):
