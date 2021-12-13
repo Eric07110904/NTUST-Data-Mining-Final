d56056860c6907ca219fc277f00934f5e398d67f
loli <z3310z33104107@gmail.com>
2021-12-13 15:31:51 +0800

    add incrimination function & result

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,3 +3,4 @@ from utils.x_sq_generation import *
 from utils.maximum_weight import *
 from utils.cvx import * 
 from utils.draw import *
+from utils.incrimination import *
---
+++ utils/incrimination.py
@@ -0,0 +1,45 @@
+import numpy as np 
+
+def incrimination(C: list, X: np.ndarray, b: int):
+    S = []
+    p = np.copy(X[:])
+    pre_f_S =  0
+    while len(S) < b:
+        f_S = 0
+        max_index = -1
+        delta_f = -100
+        for i in C:
+            temp = np.argmax(p[i])
+            S.append(temp)
+            f_S_temp = 0
+            for j in C:
+                f_S_temp += np.max(X[j][S])
+            if delta_f < f_S_temp - pre_f_S:
+                max_index = np.argmax(p[j])
+                delta_f = f_S_temp - pre_f_S
+                f_S = f_S_temp
+            S = S[:-1]
+        S.append(max_index)
+        p[:, S] = -100
+        delta_f = f_S - pre_f_S
+        pre_f_S = f_S
+    #print("incrimination: ", pre_f_S)
+    return f_S
+
+def get_incrimination(C: list, X: np.ndarray, n: int, W_t: np.ndarray, fp_num):
+    max_index = []
+    for i in range(W_t.shape[0]):
+        max_index.append(np.argsort(W_t[i])[-fp_num:])
+    max_index = np.array(max_index)
+    for index ,clu_i in enumerate(C):
+        oss_fp_j_j_weight = []
+        oss_fp_j_j_normal = []
+        f_S_max = np.array(incrimination(clu_i, X, n))
+        for b in range(1, fp_num + 1):
+            f_S_max_wei = np.array(incrimination(clu_i, X[:, max_index[index, -b: ]], b))
+            f_S_normal = np.array(incrimination(clu_i, X, b ))
+            oss_fp_j_j_weight.append(f_S_max_wei / f_S_max)
+            oss_fp_j_j_normal.append(f_S_normal / f_S_max)
+        print("------------------")
+        print(oss_fp_j_j_weight)
+        print(oss_fp_j_j_normal)
